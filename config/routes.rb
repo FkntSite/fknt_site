9f104c2717def5458c8f58c3b3115c2e81899c0d
mechetel <dima.homa5@gmail.com>
2018-11-24 18:00:46 +0200

    rename News controller to Post + start creating model of posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  get 'history' => 'pages#history'
-  get 'pmi' => 'pages#pmi'
-  get 'dean' => 'pages#dean'
-  get 'kn' => 'pages#kn'
-  get 'philosophi' => 'pages#philosophi'
-  get 'schedule' => 'pages#schedule'
+  get 'history'       => 'pages#history'
+  get 'pmi'           => 'pages#pmi'
+  get 'dean'          => 'pages#dean'
+  get 'kn'            => 'pages#kn'
+  get 'philosophi'    => 'pages#philosophi'
+  get 'schedule'      => 'pages#schedule'
   get 'consultations' => 'pages#consultations'
-  get 'proces' => 'pages#proces'
-  get 'plans' => 'pages#plans'
+  get 'proces'        => 'pages#proces'
+  get 'plans'         => 'pages#plans'
   get 'organizations' => 'pages#organizations'
-  get 'documents' => 'pages#documents'
-  get 'specialties' => 'pages#specialties'
-  get 'aspirantur' => 'pages#aspirantur'
-  get 'cooperations' => 'pages#cooperations'
-  get 'contacts' => 'pages#contacts'
+  get 'documents'     => 'pages#documents'
+  get 'specialties'   => 'pages#specialties'
+  get 'aspirantur'    => 'pages#aspirantur'
+  get 'cooperations'  => 'pages#cooperations'
+  get 'contacts'      => 'pages#contacts'
+
+  resources :posts
+  get 'index' => 'posts#index'
 
 end
