d427debb5c160a2967cd6a9eda7339da2ab7a035
mechetel <dima.homa5@gmail.com>
2018-11-24 16:30:35 +0200

    add Ki controller + add routes and html templates

---
+++ app/controllers/ki_controller.rb
@@ -0,0 +1,2 @@
+class KiController < ApplicationController
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,65 @@ class PagesController < ApplicationController
   def index
 
   end
+
+  def history
+
+  end
+
+  def dean
+
+  end
+
+  def pmi
+
+  end
+
+  def kn
+
+  end
+
+  def philosophy
+
+  end
+
+  def schedule
+
+  end
+
+  def consultation
+
+  end
+
+  def proces
+
+  end
+
+  def plans
+
+  end
+
+  def organizations
+
+  end
+
+  def documents
+
+  end
+
+  def specialties
+
+  end
+
+  def aspirantur
+
+  end
+
+  def cooperations
+
+  end
+
+  def contacts
+
+  end
+
 end
